39a76d09a601de6e09025f012cd6dd93a65aeaec
Mangesh Pise <mppise@gmail.com>
2024-06-02 19:43:06 -0400

    ds

--- script/default.js
+++ script/default.js
@@ -1,14 +1,18 @@
 const subscribe = () => {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
-  document.getElementById('btnsave').disabled = true;
+  document.getElementById('btnsave').hidden = true;
   fetch("https://aidapter-7yg2a2s6sq-uc.a.run.app/subscribe?app_name=BroadAI&subscriber_name=" + encodeURI(name) + "&subscriber_email=" + encodeURI(email), {
     "method": "GET",
     "headers": {
       "Content-Type": "application/json"
     }
   }).then((resp) => {
-    console.log(resp.data);
+    document.getElementById('btnsave').hidden = false;
+    if (resp.error)
+      alert("Oops! something went wrong. Can you please try again?");
+    else
+      document.getElementById('lead').innerHTML = "Welcome to the community! We will stay in touch.";
   });
 
 }; // subscribe
