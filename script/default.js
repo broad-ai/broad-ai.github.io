e322988b668ac6a6f71b4defdc7736ae427a1d59
Mangesh Pise <mppise@gmail.com>
2024-06-02 18:25:21 -0400

    ds

--- script/default.js
+++ script/default.js
@@ -2,7 +2,7 @@ const subscribe = () => {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   document.getElementById('btnsave').setAttribute('disabled', 'true');
-  fetch("https://aidapter-7yg2a2s6sq-uc.a.run.app/subscribe?app_name=BroadAI&aiprovider=GoogleAI&subscriber_name=" + encodeURI(name) + "&subscriber_email=" + encodeURI(email), {
+  fetch("https://aidapter-7yg2a2s6sq-uc.a.run.app/subscribe?app_name=BroadAI&subscriber_name=" + encodeURI(name) + "&subscriber_email=" + encodeURI(email), {
     "method": "GET",
     "headers": {
       "Content-Type": "application/json"
@@ -12,7 +12,7 @@ const subscribe = () => {
     if (resp.error)
       alert(name + " | " + email + ", there was a problem with your request.\nCan you please try again?");
     else
-      document.getElementById('lead').innerHTML = resp.message;
+      document.getElementById('lead').innerHTML = "Thank you! We will stay in touch.";
   });
 
 }; // subscribe
