b9c1e79684aa507a708e0ae705fb88add4af0732
Mangesh Pise <mppise@gmail.com>
2024-06-08 16:09:31 -0400

    sdfsdf

--- script/default.js
+++ script/default.js
@@ -4,17 +4,22 @@ const subscribe = () => {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   document.getElementById('btnsave').hidden = true;
-  fetch(broadAIapiEndpoint + "/subscribe?subscriber_name=" + encodeURI(name) + "&subscriber_email=" + encodeURI(email), {
-    "method": "GET",
+  fetch(broadAIapiEndpoint + "/subscribe", {
+    "method": "POST",
     "headers": {
       "Content-Type": "application/json"
-    }
-  }).then((resp) => {
-    document.getElementById('btnsave').hidden = false;
-    if (resp.error)
-      alert("Oops! something went wrong. Can you please try again?");
-    else
-      document.getElementById('lead').innerHTML = "<h2 style='text-align:center;'>Welcome to the community, " + name + "!</h2> <p style='text-align:center;font-size:1.25em;'>Let's stay in touch.</p>";
-  });
+    },
+    body: JSON.stringify({
+      "name": name,
+      "email": email,
+    })
+  }).then((response) => response.json())
+    .then((resp) => {
+      document.getElementById('btnsave').hidden = false;
+      if (resp.error)
+        alert("Oops! something went wrong. Can you please try again?");
+      else
+        document.getElementById('lead').innerHTML = "<h2 style='text-align:center;'>Welcome to the community, " + name + "!</h2> <p style='text-align:center;font-size:1.25em;'>Let's stay in touch.</p>";
+    });
 
 }; // subscribe
