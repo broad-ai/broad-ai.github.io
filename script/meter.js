901c1ed16fd7830881a4c80fb91632dd3892f73c
Mangesh Pise <mppise@gmail.com>
2024-07-03 18:03:45 -0400

    sdf

--- script/meter.js
+++ script/meter.js
@@ -4,26 +4,28 @@ const broadAIapiEndpoint = "http://localhost:8080";
 const fetchUsage = () => {
  const searchParams = new URLSearchParams(window.location.search);
  let appid = searchParams.get('appid');
- document.getElementById('dashboard').innerHTML = "...";
- fetch(broadAIapiEndpoint + '/app/metering/' + appid, {
-  method: 'GET',
-  headers: {
-   'Content-Type': 'application/json',
-  }
- }).then((response) => response.json())
-  .then((metrics) => {
-   document.getElementById('dashboard').innerHTML = "<div class='container'>";
-   Object.keys(metrics).forEach((agent) => {
-    document.getElementById('dashboard').innerHTML += "<div class='card'>";
-    document.getElementById('dashboard').innerHTML += "<h2>" + agent + "</h2>";
-    Object.keys(metrics[agent]).forEach((skill) => {
-     document.getElementById('dashboard').innerHTML += "<div class='sub-category'>";
-     document.getElementById('dashboard').innerHTML += "<span style='float:left;'>" + skill + "</span>";
-     document.getElementById('dashboard').innerHTML += "<span style='float:right;font-size:3em;font-weight:bold;'>" + metrics[agent][skill] + "</span>";
+ setTimeout(() => {
+  document.getElementById('dashboard').innerHTML = "...";
+  fetch(broadAIapiEndpoint + '/app/metering/' + appid, {
+   method: 'GET',
+   headers: {
+    'Content-Type': 'application/json',
+   }
+  }).then((response) => response.json())
+   .then((metrics) => {
+    document.getElementById('dashboard').innerHTML = "<div>";
+    Object.keys(metrics).forEach((agent) => {
+     document.getElementById('dashboard').innerHTML += "<div class='card'>";
+     document.getElementById('dashboard').innerHTML += "<h2>" + agent + "</h2>";
+     Object.keys(metrics[agent]).forEach((skill) => {
+      document.getElementById('dashboard').innerHTML += "<div class='sub-category'>";
+      document.getElementById('dashboard').innerHTML += "<span style='float:left;'>" + skill + "</span>";
+      document.getElementById('dashboard').innerHTML += "<span style='float:right;font-size:3em;font-weight:bold;'>" + metrics[agent][skill] + "</span>";
+      document.getElementById('dashboard').innerHTML += "</div>";
+     });
      document.getElementById('dashboard').innerHTML += "</div>";
     });
     document.getElementById('dashboard').innerHTML += "</div>";
    });
-   document.getElementById('dashboard').innerHTML += "</div>";
-  });
+ }, 600);
 };
