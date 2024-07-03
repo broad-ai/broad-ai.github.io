53b341730d3093edde06ebf11776a71856750205
Mangesh Pise <mppise@gmail.com>
2024-07-03 16:49:57 -0400

    sdfdsf

--- metering.md
+++ metering.md
@@ -2,3 +2,5 @@
 layout: metering
 title: Dashboard | BroadAI Usage
 ---
+
+<div id="usage"> <!-- --> </div>
--- script/meter.js
+++ script/meter.js
@@ -1,5 +1,17 @@
+const broadAIapiEndpoint = "https://broadai-7yg2a2s6sq-uc.a.run.app";
+
 const getDetails = () => {
  const searchParams = new URLSearchParams(window.location.search);
- console.log(searchParams);
- console.log(searchParams.get('appid'));
+
+ let appid = searchParams.get('appid');
+ fetch(broadAIapiEndpoint + '/app/metering/' + appid, {
+  method: "GET",
+  headers: {
+   "Content-Type": "application/json",
+  }
+ }).then((response) => response.json())
+  .then((resp) => {
+   console.log(resp.data);
+  });
+
 };
