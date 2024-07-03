f69f35ef6e7eb7a203603cb2f5a952c670daa3ca
Mangesh Pise <mppise@gmail.com>
2024-07-03 17:17:18 -0400

    ghfg

--- script/meter.js
+++ script/meter.js
@@ -1,35 +1,35 @@
-const broadAIapiEndpoint = "http://localhost:8080";
-// const broadAIapiEndpoint = "https://broadai-7yg2a2s6sq-uc.a.run.app";
+const broadAIapiEndpoint = 'http://localhost:8080';
+// const broadAIapiEndpoint = 'https://broadai-7yg2a2s6sq-uc.a.run.app';
 
 const getDetails = () => {
  const searchParams = new URLSearchParams(window.location.search);
 
  let appid = searchParams.get('appid');
  fetch(broadAIapiEndpoint + '/app/metering/' + appid, {
-  method: "GET",
+  method: 'GET',
   headers: {
-   "Content-Type": "application/json",
+   'Content-Type': 'application/json',
   }
  }).then((response) => response.json())
   .then((resp) => {
    console.log(resp);
    let metrics = resp;
-   let usage = '<div class="container">';
+   let usage = "<div class='container'>";
    console.log(metrics);
    Object.keys(metrics).forEach((agent) => {
     console.log(agent, metrics[agent]);
-    usage += '<div class="card">';
-    usage += '<h2>' + agent + '</h2>';
+    usage += "<div class='card'>";
+    usage += "<h2>" + agent + "</h2>";
     Object.keys(metrics[agent]).forEach((skill) => {
      console.log(skill, metrics[agent][skill]);
-     usage += '<div class="sub-category">';
-     usage += '<span style="float:left;">' + skill + '</span>';
-     usage += '<span style="float:right;font-size:3em;font-weight:bold;">' + metrics[agent][skill] + '</span>';
-     usage += '</div>';
+     usage += "<div class='sub-category'>";
+     usage += "<span style='float:left;'>" + skill + "</span>";
+     usage += "<span style='float:right;font-size:3em;font-weight:bold;'>" + metrics[agent][skill] + "</span>";
+     usage += "</div>";
     });
-    usage += '</div>';
+    usage += "</div>";
    });
-   usage += '</div>';
+   usage += "</div>";
   });
  console.log(usage);
  document.getElementById('usage').innerHTML = usage;
