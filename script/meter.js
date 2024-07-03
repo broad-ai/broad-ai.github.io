29bd7e3f7381df3ae210ab9150704fdaec10b0fb
Mangesh Pise <mppise@gmail.com>
2024-07-03 19:41:29 -0400

    sdfsd

--- script/meter.js
+++ script/meter.js
@@ -1,22 +1,25 @@
-const broadAIapiEndpoint = "http://localhost:8080";
-// const broadAIapiEndpoint = "https://broadai-7yg2a2s6sq-uc.a.run.app";
+// const broadAIapiEndpoint = "http://localhost:8080";
+const broadAIapiEndpoint = "https://broadai-7yg2a2s6sq-uc.a.run.app";
 
 const fetchUsage = () => {
- const searchParams = new URLSearchParams(window.location.search);
- let appid = searchParams.get('appid');
- let html = "...";
- setTimeout(() => {
-  document.getElementById('dashboard').innerHTML = html;
-  fetch(broadAIapiEndpoint + '/app/metering/' + appid, {
-   method: 'GET',
-   headers: {
-    'Content-Type': 'application/json',
-   }
-  }).then((response) => response.json())
-   .then((metrics) => {
-    html = JSON.stringify(metrics, null, 4);
-
+  const searchParams = new URLSearchParams(window.location.search);
+  let appid = searchParams.get('appid');
+  let html = "...";
+  setTimeout(() => {
     document.getElementById('dashboard').innerHTML = html;
-   });
- }, 600);
+    fetch(broadAIapiEndpoint + '/app/metering/' + appid, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then((response) => response.json())
+      .then((metrics) => {
+        Object.keys(metrics).forEach((agent) => {
+          html += "<h2>" + agent + "</h2>";
+          html += "<code>" + JSON.stringify(metrics[agent]) + "</code>";
+          html += "<p></p>";
+        });
+        document.getElementById('dashboard').innerHTML = html;
+      });
+  }, 600);
 };
