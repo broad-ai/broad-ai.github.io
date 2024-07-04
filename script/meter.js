8791b972e7b4ffda14f53b8b9c10beda447db4d3
Mangesh Pise <mppise@gmail.com>
2024-07-04 08:48:41 -0400

    sdf

--- script/meter.js
+++ script/meter.js
@@ -13,11 +13,16 @@ const fetchUsage = () => {
       }
     }).then((response) => response.json())
       .then((metrics) => {
-        Object.keys(metrics).forEach((agent) => {
-          html = "<h2>" + agent + "</h2>";
-          html += "<code>" + JSON.stringify(metrics[agent], null, 4) + "</code>";
-          html += "<p></p>";
-        });
+        let agents = Object.keys(metrics);
+        if (agents.length) {
+          agents.forEach((agent) => {
+            html = "<h2>" + agent + "</h2>";
+            html += "<code>" + JSON.stringify(metrics[agent], null, 4) + "</code>";
+            html += "<p></p>";
+          });
+        }
+        else
+          html = "<p>No usage, so far...</p> <p>Please refer to the <a href='/docu-mas.html'>documentation</a> to learn how to use BroadAI to build next-gen AI applications, or feel free to <img src='./assets/images/icon-rocket.png' style='height:1.5em; padding: 0; margin: 0; '><a href='mailto:broad.agents.ai@gmail.com?subject=Re%20using%20my%20BroadAI%20App%20ID'reach out to us</a>.</p>";
         document.getElementById('dashboard').innerHTML = html;
       });
   }, 600);
