61de3a45fcef0970aede1f368a9183160c58ca71
Mangesh Pise <mppise@gmail.com>
2024-07-14 00:01:40 -0400

    fdgfd

--- script/demo.js
+++ script/demo.js
@@ -214,7 +214,7 @@ Use exactly the following JSON structure to generate your final response (note t
                 </div>
               </div>
               <div class="card">
-                <img src="`+ recommendation.poster + `" onerror="` + ['https://cdn.pixabay.com/photo/2015/10/04/23/04/popcorn-972047_1280.png', 'https://cdn.pixabay.com/photo/2012/04/13/21/17/ticket-33657_1280.png', 'https://cdn.pixabay.com/photo/2015/08/20/21/13/popcorn-898154_1280.png', 'https://cdn.pixabay.com/photo/2014/12/22/00/01/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)] + `" class="card-img-top" alt="...">
+                <img src="`+ recommendation.poster + `" onerror="` + ['assets/popcorn-972047_1280.png', 'assets/ticket-33657_1280.png', 'assets/popcorn-898154_1280.png', 'assets/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)] + `" class="card-img-top" alt="...">
                 <div class="card-body">
                   <p class="row card-text">
                     <span class="col-6 text-left">
@@ -245,7 +245,7 @@ Use exactly the following JSON structure to generate your final response (note t
         html += `
             <div class="col-12 col-md-4">
               <div class="card">
-                <img src="`+ ['https://cdn.pixabay.com/photo/2015/10/04/23/04/popcorn-972047_1280.png', 'https://cdn.pixabay.com/photo/2012/04/13/21/17/ticket-33657_1280.png', 'https://cdn.pixabay.com/photo/2015/08/20/21/13/popcorn-898154_1280.png', 'https://cdn.pixabay.com/photo/2014/12/22/00/01/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)] + `" class="card-img-top" alt="...">
+                <img src="`+ ['assets/popcorn-972047_1280.png', 'assets/ticket-33657_1280.png', 'assets/popcorn-898154_1280.png', 'assets/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)] + `" class="card-img-top" alt="...">
                 <div class="card-body">
                   <p class="row card-text">
                     <span class="col-6 text-left">
@@ -320,7 +320,7 @@ Use exactly the following JSON structure to generate your final response (note t
       document.getElementById('pickTitle').innerHTML = details.title;
       document.getElementById('pickDirector').innerHTML = details.director;
       document.getElementById('pickPoster').setAttribute('src', details.poster);
-      document.getElementById('pickPoster').setAttribute('onerror', ['https://cdn.pixabay.com/photo/2015/10/04/23/04/popcorn-972047_1280.png', 'https://cdn.pixabay.com/photo/2012/04/13/21/17/ticket-33657_1280.png', 'https://cdn.pixabay.com/photo/2015/08/20/21/13/popcorn-898154_1280.png', 'https://cdn.pixabay.com/photo/2014/12/22/00/01/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)]);
+      document.getElementById('pickPoster').setAttribute('onerror', ['assets/popcorn-972047_1280.png', 'assets/ticket-33657_1280.png', 'assets/popcorn-898154_1280.png', 'assets/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)]);
       document.getElementById('pickRating').innerHTML = details.imdb_rating;
       document.getElementById('pickYear').innerHTML = details.year;
 
