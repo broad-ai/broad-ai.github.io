66bbde65911b50debdef70f3f12c4563b1709f0a
Mangesh Pise <mppise@gmail.com>
2024-07-14 03:02:32 -0400

    fsdf

--- script/demo.js
+++ script/demo.js
@@ -214,7 +214,7 @@ Prepare exactly the following stringified JSON structure to generate your final 
                 </div>
               </div>
               <div class="card">
-                <img src="`+ recommendation.poster.indexOf('http') >= 0 ? recommendation.poster : ['assets/images/popcorn-972047_1280.png', 'assets/images/ticket-33657_1280.png', 'assets/images/popcorn-898154_1280.png', 'assets/images/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)] + `" class="card-img-top" alt="...">
+                <img src="`+ recommendation.poster + `" onerror="this.src='` + ['assets/images/popcorn-972047_1280.png', 'assets/images/ticket-33657_1280.png', 'assets/images/popcorn-898154_1280.png', 'assets/images/popcorn-576599_1280.png'][Math.floor(Math.random() * 4)] + `'" class="card-img-top" alt="...">
                 <div class="card-body">
                   <p class="row card-text">
                     <span class="col-6 text-left">
