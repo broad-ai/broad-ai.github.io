8c15a648b8de037066613b95f5e97a2fce3b6ca2
Mangesh Pise <mppise@gmail.com>
2024-07-30 22:58:26 -0400

    sdf

--- script/demo.js
+++ script/demo.js
@@ -1,5 +1,4 @@
-// const broadAIapiEndpoint = "https://broadaidemo-7yg2a2s6sq-uc.a.run.app";
-const broadAIapiEndpoint = "http://localhost:8080";
+const broadAIapiEndpoint = "https://broadaidemo-7yg2a2s6sq-uc.a.run.app";
 
 // ------ ..... ------ ..... ------ ..... ------ 
 const goChatbot = () => {
