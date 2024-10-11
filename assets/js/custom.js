155f172aa2296b8b3f1bfd76d3f8d54386046a87
Mangesh Pise <mppise@gmail.com>
2024-10-11 10:27:29 -0400

    sdf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -193,9 +193,9 @@ const subscribe = () => {
       .then((resp) => {
         document.getElementById('btnsubscribe').disabled = false;
         if (resp.error)
-          document.getElementById('subscribed').innerHTML = "<h6 class='px-1 py-1' style='background-color:#00500d;color:#fff;'>Oops! " + name + ",</h6> <p>" + resp.reason + "</p>";
+          document.getElementById('subscribed').innerHTML = "<h6 class='p1 m-1' style='background-color:#00500d;color:#fff;'>Oops! " + name + ",</h6> <p>" + resp.reason + "</p>";
         else
-          document.getElementById('subscribed').innerHTML = "<h6 class='px-1 py-1' style='background-color:#ff654c;color:#fff;'>Welcome to the community, " + name + "!</h6> <p>Let's stay in touch.</p>";
+          document.getElementById('subscribed').innerHTML = "<h6 class='p-1 m-1' style='background-color:#ff654c;color:#fff;'>Welcome to the community, " + name + "!</h6> <p>Let's stay in touch.</p>";
       });
   }
   else {
