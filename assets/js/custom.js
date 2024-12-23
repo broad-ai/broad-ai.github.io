f2114dfd563db2855c8a94a3796fe15ad3685caa
Mangesh Pise <mppise@gmail.com>
2024-12-23 15:50:00 -0500

    sdf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -295,9 +295,9 @@ const clearChat = () => {
 
 const goChatbot = () => {
   // -- pre results formatting
-  document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre>" + getRandomMessage() + "...</pre></p></div>";
+  document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre class='text-primary'>" + getRandomMessage() + "</pre></p></div>";
   let intvlMsgs = setInterval(() => {
-    document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre>" + getRandomMessage() + "...</pre></p></div>";
+    document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre class='text-primary'>" + getRandomMessage() + "...</pre></p></div>";
   }, 10000);
   document.getElementById('btnAsk').disabled = true;
   document.getElementById('chatbox').disabled = true;
