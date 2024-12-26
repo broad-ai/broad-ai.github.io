911daf983997c2df7adf997d2d2fc76462c30344
Mangesh Pise <mppise@gmail.com>
2024-12-26 00:34:28 -0500

    zcsz

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -489,7 +489,6 @@ const goConcierge = () => {
             // -- post results formatting
             clearInterval(intvlMsgs);
             document.getElementById('btnGoConcierge').disabled = false;
-            document.getElementById('origin').value = "";
             document.getElementById('destination').value = "";
             document.getElementById('notes').value = "";
             return;
@@ -598,10 +597,9 @@ const goMovies = () => {
     document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre class='text-primary'>" + getRandomMessage() + "</pre></p></div>";
   }, 10000);
 
-
   // -- get location data for weather and news
   fetch("https://ipinfo.io/json").then((resp) => resp.json()).then((geo) => {
-    fetch(broadAIDemoapiEndpoint + '/recommend', {
+    fetch(broadAIDemoapiEndpoint + '/movie/recommend', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -639,24 +637,32 @@ const goMovies = () => {
                   else
                     messages += "<" + line.html_tag + " style='text-align:left;color:#6a5acd;'>" + line.text + "</" + line.html_tag + ">";
                 });
-                messages += "<hr> <div class='row p-3'>";
+                messages += "<hr> <div class='row p-1'>";
                 movies.forEach((movie) => {
                   let movieCard = `
-                  <div class="card col-12 col-sm-6 col-md-4 col-lg-4 m-1">
-                    <img src="`+ movie.image + `" class="card-img-top" alt="` + movie.name + `">
-                    <div class="card-body">
-                      <h2 class="card-title">`+ movie.name + `</h2>
-                      <h4 class="card-subtitle mb-2 text-muted">`+ movie.director + `</h4>
-                      <hr>
-                      <div class='row'>
-                        <div class='col text-start'><span class='text-muted'>Year: </span>`+ movie.year + `</div>
-                        <div class='col text-end'><span class='text-muted'>Rating: </span>`+ movie.rating + `</div>
-                      </div>
-                      <hr>
-                      <p class="card-text text-muted">`+ movie.plot + `</p>
-                    </div>
-                  </div>
-                                    `;
+<div class="col-12 col-sm-12 col-md-6 col-lg-6">
+  <div class="card">
+    <img src="`+ movie.image + `" class="card-img-top" alt="` + movie.name + `">
+    <div class="card-body">
+      <h2 class="card-title">`+ movie.name + `</h2>
+      <h4 class="card-subtitle mb-2 text-muted">`+ movie.director + `</h4>
+      <hr>
+      <div class='row'>
+        <div class='col text-start'><span class='text-muted'>Release Year: </span>`+ movie.year + `</div>
+        <div class='col text-end'><span class='text-muted'>IMDB Rating: </span>`+ movie.rating + `</div>
+      </div>
+      <hr>  
+      <div class='row'>
+        <div class='col'><span class='text-muted'>`+ movie.plot + `</span></div>
+      </div>
+      <hr>  
+      <div class='row'>
+        <div class='col text-center'><a href='javascript:goSimilarMovies(\"`+ encodeURI(movie.name) + `\", \"` + encodeURI(movie.director) + `\", \"` + movie.year + `\", \"` + movie.rating + `\");'>Find similar</a></div>
+      </div>
+    </div>
+  </div>
+</div>
+                  `;
                   messages += movieCard;
                 });
                 messages += "</div>"
@@ -674,84 +680,84 @@ const goMovies = () => {
             }
             if (payload.result) {
               let logs = `
-            <table class='table mb-1'>
-              <tbody>
-                <tr>
-                  <td><strong>Status:</strong> <span class='text-danger'>`+ payload.status + `</span></td>
-                </tr>
-              </tbody>
-            </table>`;
+          <table class='table mb-1'>
+            <tbody>
+              <tr>
+                <td><strong>Status:</strong> <span class='text-danger'>`+ payload.status + `</span></td>
+              </tr>
+            </tbody>
+          </table>`;
               if (payload.result.plan) {
                 payload.result.plan.forEach((step) => {
                   logs += `
-              <table class='table mt-1'>
-                <thead>
-                  <tr>
-                  <th>`+ (step.sequence + 1) + `.  <span class='text-info'>` + step.objective + `</span></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class='py-0'><span class='text-muted'>Agent &rarr; Skill</span>&emsp;|&emsp;<span class='text-success'>`+ step.agent + ` </span> &rarr; <span class='text-success'>` + step.skill.name + `</span></td>
-                  </tr>
-                  <tr>
-                    <td class='text-muted'>`+ (step.result ? (typeof step.result == 'object' ? JSON.stringify(step.result) : step.result) : '...') + `</td>
-                  </tr>
-                </tbody>
-              </table>
-                `;
+            <table class='table mt-1'>
+              <thead>
+                <tr>
+                <th>`+ (step.sequence + 1) + `.  <span class='text-info'>` + step.objective + `</span></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class='py-0'><span class='text-muted'>Agent &rarr; Skill</span>&emsp;|&emsp;<span class='text-success'>`+ step.agent + ` </span> &rarr; <span class='text-success'>` + step.skill.name + `</span></td>
+                </tr>
+                <tr>
+                  <td class='text-muted'>`+ (step.result ? (typeof step.result == 'object' ? JSON.stringify(step.result) : step.result) : '...') + `</td>
+                </tr>
+              </tbody>
+            </table>
+              `;
                 });
               }
               else if (payload.result.agents) {
                 let agents = `
-              <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
-                <h3 style='margin-top:auto;margin-bottom:auto;'>
-                  Available Agents:
-                </h3>
-              </div>
-                `;
+            <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+              <h3 style='margin-top:auto;margin-bottom:auto;'>
+                Available Agents:
+              </h3>
+            </div>
+              `;
                 payload.result.agents.forEach((agent) => {
                   agents += `
-            <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
-              <table class='table mt-1'>
-                <thead>
-                  <tr>
-                  <th><strong>Agent:</strong> <span class='text-info'>` + agent.agent + `</span></th>
-                  </tr>
-                  <tr>
-                  <td>` + agent.capability + `</span></td>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td class='text-muted'>
-                      <ol>`;
+          <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <table class='table mt-1'>
+              <thead>
+                <tr>
+                <th><strong>Agent:</strong> <span class='text-info'>` + agent.agent + `</span></th>
+                </tr>
+                <tr>
+                <td>` + agent.capability + `</span></td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class='text-muted'>
+                    <ol>`;
                   agent.skills.forEach((skill) => {
                     agents += `
-                  <li class='mb-1'><strong>`+ skill.skill + `</strong>: <span class='text-meta'>` + skill.objective + `</span></li>
-                  `;
+                <li class='mb-1'><strong>`+ skill.skill + `</strong>: <span class='text-meta'>` + skill.objective + `</span></li>
+                `;
                   });
                   agents += `
-                        </ol>
-                      </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-                `;
+                      </ol>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+              `;
                 });
                 document.getElementById('agents').innerHTML = agents;
               }
               else {
                 logs += `
-              <table class='table mt-1'>
-                <tbody>
-                  <tr>
-                    <td class='text-muted'><pre>`+ JSON.stringify(payload.result, null, 2) + `</pre></td>
-                  </tr>
-                </tbody>
-              </table>
-                `;
+            <table class='table mt-1'>
+              <tbody>
+                <tr>
+                  <td class='text-muted'><pre>`+ JSON.stringify(payload.result, null, 2) + `</pre></td>
+                </tr>
+              </tbody>
+            </table>
+              `;
               }
               document.getElementById('logs').innerHTML = logs;
             }
@@ -762,6 +768,187 @@ const goMovies = () => {
       });
   });
 }; // goMovies
+
+// ------ ..... ------ ..... ------ ..... ------ 
+const goSimilarMovies = (movie, director, year, rating) => {
+  // -- pre results formatting
+  document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre class='text-primary'>" + getRandomMessage() + "</pre></p></div>";
+  let intvlMsgs = setInterval(() => {
+    document.getElementById('chat').innerHTML = "<div class='p-3'><img src='/assets/images/load-35_128.gif' style='width:60px; height:60px;'><pre class='text-primary'>" + getRandomMessage() + "</pre></p></div>";
+  }, 10000);
+
+  // -- get location data for weather and news
+  fetch("https://ipinfo.io/json").then((resp) => resp.json()).then((geo) => {
+    fetch(broadAIDemoapiEndpoint + '/movie/similar', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "movie": movie,
+        "director": director,
+        "year": year,
+        "rating": rating
+      })
+    })
+      .then((resp) => {
+        let reader = resp.body.pipeThrough(new TextDecoderStream()).getReader();
+        let payload = {};
+        const readStream = (reader) => {
+          reader.read().then((r) => {
+            if (r.done) {
+              let movies = [];
+
+              // -- showing results
+              let messages = "<div style='text-align:right;margin-bottom:1em;'><a href='javascript:clearChat();'>Clear</a></pre></div>"
+              if (payload.result.response) {
+                payload.result.response.forEach((line) => {
+                  // -- extracting JSON response from results
+                  let jsnMovies = { "movies": [] };
+                  if (line.text.indexOf('{') >= 0 && line.text.lastIndexOf('}') > 0) {
+                    try {
+                      jsnMovies = JSON.parse(line.text.substring(line.text.indexOf('{'), line.text.lastIndexOf('}') + 1).replaceAll(new RegExp('/\\*[\\s\\S]*?\\*/', 'gm'), ''));
+                    }
+                    catch {
+                      console.log(line.text);
+                    }
+                    if (jsnMovies.movies)
+                      jsnMovies.movies.forEach((movie) => movies.push(movie));
+                  }
+                  else
+                    messages += "<" + line.html_tag + " style='text-align:left;color:#6a5acd;'>" + line.text + "</" + line.html_tag + ">";
+                });
+                messages += "<hr> <div class='row p-1'>";
+                movies.forEach((movie) => {
+                  let movieCard = `
+<div class="col-12 col-sm-12 col-md-6 col-lg-6">
+  <div class="card">
+    <img src="`+ movie.image + `" class="card-img-top" alt="` + movie.name + `">
+    <div class="card-body">
+      <h2 class="card-title">`+ movie.name + `</h2>
+      <h4 class="card-subtitle mb-2 text-muted">`+ movie.director + `</h4>
+      <hr>
+      <div class='row'>
+        <div class='col text-start'><span class='text-muted'>Release Year: </span>`+ movie.year + `</div>
+        <div class='col text-end'><span class='text-muted'>IMDB Rating: </span>`+ movie.rating + `</div>
+      </div>
+      <hr>  
+      <div class='row'>
+        <div class='col'><span class='text-muted'>`+ movie.plot + `</span></div>
+      </div>
+      <hr>  
+      <div class='row'>
+        <div class='col text-center'><a href='javascript:goSimilarMovies(\"`+ encodeURI(movie.name) + `\", \"` + encodeURI(movie.director) + `\", \"` + movie.year + `\", \"` + movie.rating + `\");'>Find similar</a></div>
+      </div>
+    </div>
+  </div>
+</div>
+                  `;
+                  messages += movieCard;
+                });
+                messages += "</div>"
+              }
+              document.getElementById('chat').innerHTML = messages;
+              // -- post results formatting
+              clearInterval(intvlMsgs);
+              return;
+            }
+            try {
+              payload = JSON.parse(r.value)
+            }
+            catch {
+              payload = { status: "Please wait...", result: null };
+            }
+            if (payload.result) {
+              let logs = `
+          <table class='table mb-1'>
+            <tbody>
+              <tr>
+                <td><strong>Status:</strong> <span class='text-danger'>`+ payload.status + `</span></td>
+              </tr>
+            </tbody>
+          </table>`;
+              if (payload.result.plan) {
+                payload.result.plan.forEach((step) => {
+                  logs += `
+            <table class='table mt-1'>
+              <thead>
+                <tr>
+                <th>`+ (step.sequence + 1) + `.  <span class='text-info'>` + step.objective + `</span></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class='py-0'><span class='text-muted'>Agent &rarr; Skill</span>&emsp;|&emsp;<span class='text-success'>`+ step.agent + ` </span> &rarr; <span class='text-success'>` + step.skill.name + `</span></td>
+                </tr>
+                <tr>
+                  <td class='text-muted'>`+ (step.result ? (typeof step.result == 'object' ? JSON.stringify(step.result) : step.result) : '...') + `</td>
+                </tr>
+              </tbody>
+            </table>
+              `;
+                });
+              }
+              else if (payload.result.agents) {
+                let agents = `
+            <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+              <h3 style='margin-top:auto;margin-bottom:auto;'>
+                Available Agents:
+              </h3>
+            </div>
+              `;
+                payload.result.agents.forEach((agent) => {
+                  agents += `
+          <div class='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <table class='table mt-1'>
+              <thead>
+                <tr>
+                <th><strong>Agent:</strong> <span class='text-info'>` + agent.agent + `</span></th>
+                </tr>
+                <tr>
+                <td>` + agent.capability + `</span></td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class='text-muted'>
+                    <ol>`;
+                  agent.skills.forEach((skill) => {
+                    agents += `
+                <li class='mb-1'><strong>`+ skill.skill + `</strong>: <span class='text-meta'>` + skill.objective + `</span></li>
+                `;
+                  });
+                  agents += `
+                      </ol>
+                    </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+              `;
+                });
+                document.getElementById('agents').innerHTML = agents;
+              }
+              else {
+                logs += `
+            <table class='table mt-1'>
+              <tbody>
+                <tr>
+                  <td class='text-muted'><pre>`+ JSON.stringify(payload.result, null, 2) + `</pre></td>
+                </tr>
+              </tbody>
+            </table>
+              `;
+              }
+              document.getElementById('logs').innerHTML = logs;
+            }
+            readStream(reader);
+          });
+        }; // readStream
+        readStream(reader);
+      });
+  });
+}; // goSimilarMovies
 
 
 // ------ ..... ------ ..... ------ ..... ------ 
