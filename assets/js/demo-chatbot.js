e19d2c4fa0eb10c191b887ef7d911d586e3a3bb8
Mangesh Pise <mppise@gmail.com>
2025-07-12 16:06:13 -0400

    sdf

--- assets/js/demo-chatbot.js
+++ assets/js/demo-chatbot.js
@@ -1,140 +1,156 @@
 // ------ ..... ------ ..... ------ ..... ------ 
-const goChatbot = () => {
+const goChatbot = (file) => {
     let DOMResponse = document.getElementById('response');
     let DOMStatus = document.getElementById('status');
     let DOMPlan = document.getElementById('plan');
     let DOMAgents = document.getElementById('agents');
-    DOMStatus.innerHTML = "";
-    DOMPlan.innerHTML = "";
-    DOMAgents.innerHTML = "";
-    // -- pre-processing DOM adjustments
     DOMResponse.style.paddingBottom = "3em";
-    DOMResponse.innerHTML = `<img class='m-2 float-end' src='/assets/images/load-35_128.gif' style='width:3em; height:3em;'>`;
     let payload = {};
     document.getElementById('btnAsk').disabled = true;
     document.getElementById('chatbox').disabled = true;
-    // -- engage BroadAI
-    let conversation = [];
-    try { conversation = JSON.parse(sessionStorage.getItem('conversation')); }
-    catch { sessionStorage.clear('conversation'); }
-    fetch(broadAIDemoapiEndpoint + '/go', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "question": document.getElementById('chatbox').value,
-            "conversation": conversation
-        })
-    }).then((resp) => {
-        let streamReader = resp.body.pipeThrough(new TextDecoderStream()).getReader();
-        // -- function: process streamed response
-        let buffer = '';
-        const processSteam = (reader) => {
-            reader.read().then((chunk) => {
-                if (!chunk.done) {
-                    if (chunk.value.indexOf('\n') == -1)
-                        buffer += chunk.value;
-                    else {
-                        buffer += chunk.value;
-                        let cutoffat = buffer.indexOf('\n');
-                        try {
-                            let obj = JSON.parse(buffer.slice(0, cutoffat));
-                            buffer = buffer.slice(cutoffat + 1);
-                            console.log("Parsed chunk: ", obj);
-                            payload = processPayload(obj, DOMResponse, DOMStatus, DOMPlan, DOMAgents);
+    if (file) {
+        // -- engage BroadAI
+        let conversation = [];
+        fetch(broadAIDemoapiEndpoint + '/analyzefile', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "question": document.getElementById('chatbox').value,
+                "conversation": conversation
+            })
+        }).then((resp) => {
+            let streamReader = resp.body.pipeThrough(new TextDecoderStream()).getReader();
+            // -- function: process streamed response
+            let buffer = '';
+            const processSteam = (reader) => {
+                reader.read().then((chunk) => {
+                    if (!chunk.done) {
+                        if (chunk.value.indexOf('\n') == -1)
+                            buffer += chunk.value;
+                        else {
+                            buffer += chunk.value;
+                            let cutoffat = buffer.indexOf('\n');
+                            try {
+                                let obj = JSON.parse(buffer.slice(0, cutoffat));
+                                buffer = buffer.slice(cutoffat + 1);
+                                console.log("Parsed chunk: ", obj);
+                                payload = processPayload(obj, DOMResponse, DOMStatus, DOMPlan, DOMAgents);
+                            }
+                            catch {
+                                console.log("Could not parse JSON object: ", buffer, cutoffat, buffer.slice(0, cutoffat));
+                            }
                         }
-                        catch {
-                            console.log("Could not parse JSON object: ", buffer, cutoffat, buffer.slice(0, cutoffat));
-                        }
+                        processSteam(streamReader);
                     }
-                    processSteam(streamReader);
-                }
-                else {
-                    // -- post-processing DOM adjustments
-                    document.getElementById('btnAsk').disabled = false;
-                    document.getElementById('chatbox').disabled = false;
-                    document.getElementById('chatbox').value = "";
-                }
-            });
-        }; // processSteam
-        processSteam(streamReader);
-    }); // fetch
+                    else {
+                        // -- post-processing DOM adjustments
+                        document.getElementById('btnAsk').disabled = false;
+                        document.getElementById('chatbox').disabled = false;
+                        document.getElementById('chatbox').value = "";
+                    }
+                });
+            }; // processSteam
+            processSteam(streamReader);
+        }); // fetch
+    }
+    else {
+        DOMStatus.innerHTML = "";
+        DOMPlan.innerHTML = "";
+        DOMAgents.innerHTML = "";
+        // -- pre-processing DOM adjustments
+        DOMResponse.innerHTML = `<img class='m-2 float-end' src='/assets/images/load-35_128.gif' style='width:3em; height:3em;'>`;
+        // -- engage BroadAI
+        let conversation = [];
+        try { conversation = JSON.parse(sessionStorage.getItem('conversation')); }
+        catch { sessionStorage.clear('conversation'); }
+        fetch(broadAIDemoapiEndpoint + '/go', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "question": document.getElementById('chatbox').value,
+                "conversation": conversation
+            })
+        }).then((resp) => {
+            let streamReader = resp.body.pipeThrough(new TextDecoderStream()).getReader();
+            // -- function: process streamed response
+            let buffer = '';
+            const processSteam = (reader) => {
+                reader.read().then((chunk) => {
+                    if (!chunk.done) {
+                        if (chunk.value.indexOf('\n') == -1)
+                            buffer += chunk.value;
+                        else {
+                            buffer += chunk.value;
+                            let cutoffat = buffer.indexOf('\n');
+                            try {
+                                let obj = JSON.parse(buffer.slice(0, cutoffat));
+                                buffer = buffer.slice(cutoffat + 1);
+                                console.log("Parsed chunk: ", obj);
+                                payload = processPayload(obj, DOMResponse, DOMStatus, DOMPlan, DOMAgents);
+                            }
+                            catch {
+                                console.log("Could not parse JSON object: ", buffer, cutoffat, buffer.slice(0, cutoffat));
+                            }
+                        }
+                        processSteam(streamReader);
+                    }
+                    else {
+                        // -- post-processing DOM adjustments
+                        document.getElementById('btnAsk').disabled = false;
+                        document.getElementById('chatbox').disabled = false;
+                        document.getElementById('chatbox').value = "";
+                    }
+                });
+            }; // processSteam
+            processSteam(streamReader);
+        }); // fetch   
+    }
 }; // goChatbot
 
 // ------ ..... ------ ..... ------ ..... ------ 
-const processFileContents = (chunks) => {
-    document.getElementById('response').innerHTML = "";
-    let processing = false;
-    let i = -1;
-    let intvlProcessChunks = setInterval(() => {
-        if (!processing) {
-            processing = true;
-            i++;
-            if (i < chunks.length) {
-                document.getElementById('chatbox').value = `Analyze this:
->>>
-`+ chunks[i] + `
+const processFileContents = (knowledge, avgWordCountPerLine) => {
+    const MAX_TOKENS_LLM = (1024 * 128);
+    // -- gpt-4o : 128K input tokens & 16K output tokens
+    // -- assumption: 1 word = 1 token
+    let packageSize = Math.round(MAX_TOKENS_LLM / (avgWordCountPerLine * avgWordCountPerLine));
+
+    let currentPosition = -1;
+    let fileProcessing = false;
+
+    let fileProcessingIntvl = setInterval(() => {
+        if (!fileProcessing) {
+            currentPosition++;
+            fileProcessing = true;
+            if (currentPosition < knowledge.length) {
+                let chunk = "";
+                chunk = knowledge.slice(currentPosition, currentPosition + packageSize).join(' ');
+                currentPosition = currentPosition + packageSize;
+                // prepare chat
+                document.getElementById('chatbox').value = `>>>
+  `+ chunk + `
 <<<
-`;
-                goChatbot();
+Using contents provided within '>>>' and '<<<' symbols, generate a script for my podcast show where I explain the content in educational mode. Make it entertaining, conversational, and informational. Give due credit to the authors of the content, if available, or else refer to them and their work in third-person.
+    `;
+                sessionStorage.clear('conversation');
+                goChatbot(true);
                 let disabledChatboxIntvl = setInterval(() => {
                     if (document.getElementById('chatbox').disabled == false) {
                         clearInterval(disabledChatboxIntvl);
-                        processing = false;
+                        fileProcessing = false;
                     }
                 }, 100);
             }
             else {
-                clearInterval(intvlProcessChunks);
-                let metadata = [];
-                try { metadata = JSON.parse(sessionStorage.getItem('conversation')); } catch (e) { console.log(sessionStorage.getItem('conversation')); }
-                document.getElementById('chatbox').value = `Analyze and format the information below appropriately considering the type of information it contains along with any specific recommend actions that must be taken utilizing this content.
->>>`;
-                metadata.forEach((p) => {
-                    if (p.indexOf('>:'))
-                        document.getElementById('chatbox').value += p;
-                });
-                document.getElementById('chatbox').value += `
-<<<`;
-                goChatbot();
+                clearInterval(fileProcessingIntvl);
             }
         }
     }, 100);
-    //     const MAX_TOKENS_LLM = (1024 * 128);
-    //     // -- gpt-4o : 128K input tokens & 16K output tokens
-    //     // -- assumption: 1 word = 1 token
-    //     let packageSize = Math.round(MAX_TOKENS_LLM / (avgWordCountPerLine * avgWordCountPerLine));
-
-    //     let currentPosition = -1;
-    //     let fileProcessing = false;
-
-    //     let fileProcessingIntvl = setInterval(() => {
-    //         if (!fileProcessing) {
-    //             currentPosition++;
-    //             fileProcessing = true;
-    //             if (currentPosition < knowledge.length) {
-    //                 let chunk = "";
-    //                 chunk = knowledge.slice(currentPosition, currentPosition + packageSize).join(' ');
-    //                 currentPosition = currentPosition + packageSize;
-    //                 // prepare chat
-    //                 document.getElementById('chatbox').value = `>>>
-    //   `+ chunk + `
-    //   <<<
-    // Read the contents provided within '>>>' and '<<<' symbols. Analyze the content and format it most suitable for it's type. Also provide a high-level analysis of the content and make recommendations on what can be done with this data.
-    //     `;
-    //                 sessionStorage.clear('conversation');
-    //                 goChatbot();
-    //                 let disabledChatboxIntvl = setInterval(() => {
-    //                     if (document.getElementById('chatbox').disabled == false) {
-    //                         clearInterval(disabledChatboxIntvl);
-    //                         fileProcessing = false;
-    //                     }
-    //                 }, 100);
-    //             }
-    //             else {
-    //                 clearInterval(fileProcessingIntvl);
-    //             }
-    //         }
-    //     }, 100);
 } // processFileContents
+
+document.addEventListener('response-ready', (evt) => {
+    console.log(evt);
+});
